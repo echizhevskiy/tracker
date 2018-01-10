9a6f96caed1fdc9c294554ce99cc08e25d05fd49
echizhevsky <egorkachi@mail.ru>
2018-01-10 16:48:21 +0300

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,58 @@
+var myApp = angular.module('App',[]);
+
+myApp.controller('myApp',function ($scope, $http) {
+
+    $http.get('json/persons.json').then(function(response) {
+      $scope.persons = response.data;
+    });
+
+    $http.get('json/secondSprint.json').then(function(response){
+      $scope.tasks = response.data;
+    });
+
+    $http.get('json/thirdSprint.json').then(function(response){
+      $scope.ndates = response.data;
+    });
+
+    $scope.firstSprint = 'Sprint 1';
+    $scope.secondSprint = 'Sprint 2';
+    $scope.thirdSprint = 'Sprint 3';
+    $scope.release = 'First release';
+
+
+    /* $scope.formattedAddress = function(persons){
+      return persons.developername + ', ' + persons.done;
+    } */
+
+    $scope.maks = {
+        name: 'Jon Doe',
+        email: 'jondoe@mail.ru'
+    }
+
+});
+
+
+myApp.directive('myDir', function(){
+    return {
+        restrict: 'AECM',
+        templateUrl: 'directives/content.html',
+        replace: true,
+        scope: {
+          maksObject: "="
+        }
+    }
+});
+
+
+
+/* myApp.directive('myDir', function(){
+    return {
+        restrict: 'AECM',
+        templateUrl: 'directives/content.html',
+        replace: true,
+        scope: {
+          personsObject: "=",
+          formattedAddressFunction: "&"
+        }
+    }
+}); */
