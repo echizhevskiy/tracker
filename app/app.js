3c5846991cf634ef5e13d33aec39cdea4f2fea96
Egor <egorkachi@mail.ru>
2018-01-17 11:25:53 +0300

    Some changes with directive

--- app/app.js
+++ app/app.js
@@ -1,16 +1,33 @@
 var myApp = angular.module('App',[]);
 
 myApp.controller('myApp',['$scope', '$http', function ($scope, $http) {
-    var response = { };
+    var response = {};
 
-     $scope.getInfo = function(filename){
-       $http.get('json/' + filename).success(function(response) {
-         $scope.persons = response;
-          console.log($scope.persons);
+    $scope.getInfo = function(filename){
+      var obj = { content:null };
+      var promise = $http.get('json/' + filename);
+      promise.then(function(response) {
+          obj.content = response;
+        //   $scope.persons = response;
+          console.log(response);
+          console.log(obj.content);
        });
-      return response;
+      return obj.content;
     }
 
-    $scope.getInfo('persons.json');
+    $scope.testHello = function(){
+     var obj1 = { content:null };
+     obj1.content = [
+       { "developername":"Ivan", "tasknumber" : "1.1", "date" : "27.01.2018", "done":true },
+       { "developername":"Anton", "tasknumber" : "1.3", "date" : "01.02.2018", "done":false},
+       { "developername":"Vladimir", "tasknumber" : "1.5", "date" : "13.05.2018", "done":false },
+       { "developername":"Alexander", "tasknumber" : "1.6", "date" : "7.02.2018", "done":true }
+     ];
+   console.log(obj1.content);
+   console.log(obj1);
+     return obj1.content;
+   }
 
+
+    /*  $scope.a = getInfo('persons.json'); var aaaaaa = getIn  fo('persons.json'); */
 }]);
