0100f9f3981e52a43e2c7b3eebc610c426dbbff4
Egor <egorkachi@mail.ru>
2018-01-18 14:21:47 +0300

    Bug with displaying tables has been fixed

--- app/app.js
+++ app/app.js
@@ -1,33 +1,12 @@
 var myApp = angular.module('App',[]);
 
 myApp.controller('myApp',['$scope', '$http', function ($scope, $http) {
-    var response = {};
+    $scope.sprints = {tasks:null};
+  //  $scope.response = {tasks:null};
 
-    setTimeout($scope.getInfo = function(filename){
-      var obj = { content:null };
-      var promise = $http.get('json/' + filename);
-      promise.then(function(response) {
-          obj.content = response.data;
-        //   $scope.persons = response;
-          console.log(response.data);
-          console.log(obj.content);
+    $scope.getInfo = function(filename){
+      $http.get('json/' + filename).then(function(response) {
+        $scope.sprints = response.data;
        });
-      return obj.content;
-    }, 1000);
-
-    $scope.testHello = function(){
-     var obj1 = { content:null };
-     obj1.content = [
-       { "developername":"Ivan", "tasknumber" : "1.1", "date" : "27.01.2018", "done":true },
-       { "developername":"Anton", "tasknumber" : "1.3", "date" : "01.02.2018", "done":false},
-       { "developername":"Vladimir", "tasknumber" : "1.5", "date" : "13.05.2018", "done":false },
-       { "developername":"Alexander", "tasknumber" : "1.6", "date" : "7.02.2018", "done":true }
-     ];
-   console.log(obj1.content);
-   console.log(obj1);
-     return obj1.content;
-   }
-
-
-    /*  $scope.a = getInfo('persons.json'); var aaaaaa = getIn  fo('persons.json'); */
+    }
 }]);
