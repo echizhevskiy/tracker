ddc3706add9a7a45945eb9f5c0e26b1b017d0bfb
Egor <egorkachi@mail.ru>
2018-01-19 10:23:35 +0300

    Modal window is working, withouth saving info to json

--- app/app.js
+++ app/app.js
@@ -1,58 +1,17 @@
+
 var myApp = angular.module('App',[]);
 
 myApp.controller('myApp',['$scope', '$http', function ($scope, $http) {
-    var response = {};
+
 
     $scope.getInfo = function(filename){
-      var obj = { content:null };
-      var promise = $http.get('json/' + filename);
-      promise.then(function(response) {
-          obj.content = response;
-        //   $scope.persons = response;
-          console.log(response);
-          console.log(obj.content);
+      $http.get('json/' + filename).then(function(response) {
+        $scope.sprints = response.data;
        });
-      return obj.content;
     }
 
-    $scope.testHello = function(){
-     var obj1 = { content:null };
-     obj1.content = [
-       { "developername":"Ivan", "tasknumber" : "1.1", "date" : "27.01.2018", "done":true },
-       { "developername":"Anton", "tasknumber" : "1.3", "date" : "01.02.2018", "done":false},
-       { "developername":"Vladimir", "tasknumber" : "1.5", "date" : "13.05.2018", "done":false },
-       { "developername":"Alexander", "tasknumber" : "1.6", "date" : "7.02.2018", "done":true }
-     ];
-   console.log(obj1.content);
-   console.log(obj1);
-     return obj1.content;
-   }
-
-
-   $scope.getInfoFromjson = function(filename){
-     $http.get('json/' + filename).success(function(response) {
-       $scope.persons = response;
-     });
-    return response;
-  }
-
-
-
-
-
-
-   var testobj = [
-     { "developername":"Ivan", "tasknumber" : "1.1", "date" : "27.01.2018", "done":true },
-     { "developername":"Anton", "tasknumber" : "1.3", "date" : "01.02.2018", "done":false},
-     { "developername":"Vladimir", "tasknumber" : "1.5", "date" : "13.05.2018", "done":false },
-     { "developername":"Alexander", "tasknumber" : "1.6", "date" : "7.02.2018", "done":true }
-   ];
-
-   $scope.ggg = testobj;
-   
-
-    $scope.addnewdeveloper = function(name, number, adate, status){
-        $scope.persons.push({developername: name, tasknumber: number, date: adate, done: status});
+    $scope.addnewdeveloper = function(name, number, adate, status, idname){
+        $scope.sprints[idname].tasks.push({developername: name, tasknumber: number, date: adate, done: status});
         $http.post('json/persons.json', {developername: name, tasknumber: number, date: adate, done: status});
     }
 
