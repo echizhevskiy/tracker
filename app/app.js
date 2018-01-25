444fd9d67a593e99fc2107626227f5b597329d13
Egor <egorkachi@mail.ru>
2018-01-25 10:27:39 +0300

    Read data service has been done

--- app/app.js
+++ app/app.js
@@ -1,61 +1,31 @@
-
 var myApp = angular.module('App',[]);
 
-myApp.controller('myApp',['$scope', '$http', 'myService', function ($scope, $http, myService, countryservice) {
-    $scope.getInfo = function(filename){
-      $http.get('json/' + filename).then(function(response) {
-        $scope.sprints = response.data;
-       });
-    }
+myApp.controller('myApp',['$scope', '$http', 'countryservice', function ($scope, $http, countryservice) {
+    /* $scope.getInfo = function(filename){
+          $http.get('json/' + filename).then(function(response) {
+          $scope.sprints = response.data;
+          });
+    }*/
 
-    $scope.addnewdeveloper = function(name, number, adate, status, idname){
-        $scope.sprints[idname].tasks.push({developername: name, tasknumber: number, date: adate, done: status});
-      //  $http.post('json/persons.json', {developername: name, tasknumber: number, date: adate, done: status});
+$scope.addnewdeveloper = function(name, number, adate, status, idname){
+$scope.sprints[idname].tasks.push({developername: name, tasknumber: number, date: adate, done: status});
+// $http.post('json/persons.json', {developername: name, tasknumber: number, date: adate, done: status});
 
-      $http({
-        url: 'json/persons.json',
-        method: "POST",
-        data: {developername: name, tasknumber: number, date: adate, done: status}
-      });
-    }
+$http({
+  url: 'json/persons.json',
+  method: "POST",
+  data: {developername: name, tasknumber: number, date: adate, done: status}
+  });
+}
 
-
-    $scope.testname = myService.sprints;
-
-    $scope.testname1 = myService.getInfo('persons.json');
-
-    $scope.test = myService.variablename;
-
-    countryservice.getallcountries().success(function(data) {
-       $scope.countries = data;
-    });
-
+countryservice.getallcountries().then(function(resp) {
+    $scope.countries = resp.data;
+});
 
 }]);
 
-
-myApp.service('myService',['$http', function($http){
-
-  var serf = this;
-  this.getInfo = function(filename){
-     return $http.get('json/' + filename).then(function(response) {
-       return response.data;
-      });
-  }
-
-  $http.get('json/persons.json').then(function(response) {
-    this.sprints = response.data;
-    console.log(this.sprints);
-    this.variablename = this.sprints;
-    console.log(this.variablename);
-   });
-
-
-}]);
-
-
-myApp.service('countryservice', function ($http,countryservice) {
-  this.getallcountries = function () {
-      return $http.get('json/persons.json');
-  }
+myApp.service('countryservice', function ($http) {
+    this.getallcountries = function () {
+    return $http.get('json/persons.json');
+    }
 });
